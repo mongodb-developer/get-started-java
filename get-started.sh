ae9a23cb1595c1a788cfbb8c370b2182a0c275e6
Wan Bachtiar <sindbach@gmail.com>
2021-01-08 10:29:20 +1100

    Updated version substitution regex

--- get-started.sh
+++ get-started.sh
@@ -10,7 +10,7 @@ echo "Executing ... "
 docker run --rm -e MONGODB_URI=${MONGODB_URI} \
     -v "$(pwd)":/workspace \
     -w /workspace/java start-java \
-    "sed -i s#mongodb-driver-sync\<\/artifactId\>\<version\>[x0-9]\.[x0-9].[x0-9]#mongodb-driver-sync\<\/artifactId\>\<version\>${DRIVER_VERSION}#g \
+    "sed -i 's#mongodb-driver-sync</artifactId><version>[x0-9]\+\.[x0-9]\+\.[x0-9]\+#mongodb-driver-sync</artifactId><version>${DRIVER_VERSION}#g' \
     /workspace/java/pom.xml; \
     mvn -Dmaven.repo.local=/workspace/.m2/repository package; \
     java -cp ./target/start-1.0-SNAPSHOT.jar com.start.Getstarted"

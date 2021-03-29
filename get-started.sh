e20f64d7066f44cc41565cc9ce3ce19635c0a063
Wan Bachtiar <sindbach@gmail.com>
2021-03-29 16:52:55 +1100

    Updated to use GCR, and updated driver version

--- get-started.sh
+++ get-started.sh
@@ -5,11 +5,11 @@ then
     read -p "MONGODB URI (Required): " MONGODB_URI
 fi 
 
-DRIVER_VERSION=${2:-4.1.1}
+DRIVER_VERSION=${2:-4.2.2}
 echo "Executing ... "
 docker run --rm -e MONGODB_URI=${MONGODB_URI} \
     -v "$(pwd)":/workspace \
-    -w /workspace/java start-java \
+    -w /workspace/java ghcr.io/mongodb-developer/get-started-java \
     "sed -i 's#mongodb-driver-sync</artifactId><version>[x0-9]\+\.[x0-9]\+\.[x0-9]\+#mongodb-driver-sync</artifactId><version>${DRIVER_VERSION}#g' \
     /workspace/java/pom.xml; \
     mvn -Dmaven.repo.local=/workspace/.m2/repository package; \
